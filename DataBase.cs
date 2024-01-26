e6b968dffb061b8b49e18cf7301314b88141d5cb
Jakub Koza <kubulax25@gmail.com>
2024-01-26 10:38:07 +0100

    Added DataBase, Bookmark and Book classes

---
+++ DataBase.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JKLJ
+{
+    public static class DataBase
+    {
+        private static readonly string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Books", "books.json");
+
+        public static ObservableCollection<Book> Books { get; private set; }
+
+        static DataBase()
+        {
+            if(Books == null)
+            {
+                ReadDataBaseFromJsonFile();
+            }
+        }
+
+        public static void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public static void RemoveBook(Book book)
+        {
+            Books.Remove(book);
+        }
+
+        public static void SaveDataBaseToJsonFile()
+        {
+            if(File.Exists(dbPath))
+            {
+                string serializedDatabase = JsonConvert.SerializeObject(Books);
+                File.Delete(dbPath);
+                File.WriteAllText(dbPath, serializedDatabase);
+            }
+            else
+            {
+                Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Books"));
+
+                string serializedDatabase = JsonConvert.SerializeObject(new List<Book>());
+                File.WriteAllText(dbPath, serializedDatabase);
+            }
+        }
+
+        private static void ReadDataBaseFromJsonFile()
+        {
+            if (File.Exists(dbPath))
+            {
+                string serializedDatabase = File.ReadAllText(dbPath);
+
+                Books = JsonConvert.DeserializeObject<ObservableCollection<Book>>(serializedDatabase);
+            }
+            else
+            {
+                Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Books"));
+
+                string serializedDatabase = JsonConvert.SerializeObject(new ObservableCollection<Book>());
+                File.WriteAllText(dbPath, serializedDatabase);
+
+                Books = new ObservableCollection<Book>();
+            }
+        }
+
+    }
+}
